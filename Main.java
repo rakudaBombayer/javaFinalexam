f59cc5cd0508624398b6c8a93e638cbe4c142951
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2025-07-03 09:36:41 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,116 @@
+package Final;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		
+		//↓CSVのデータを読み込む(クラスかしてもいいかも)
+		
+		String csvFile = "src/Final/郵便番号データ.csv"; // 読み込むCSVファイルのパスを指定
+        String line;
+//        String delimiter = ","; // CSVファイルの区切り文字
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                for (String value : values) {
+                    System.out.println(value); // 読み取ったデータを表示
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace(); // エラーメッセージを表示
+        }
+        
+        //↓郵便番号検索か住所検索かの分岐(クラスかしてもいいかも)
+          
+        
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        
+      //再度入力を求めるためのwhile文
+         while (true) {
+        
+        		while (true) {
+        			System.out.println("検索条件を選択(1･･･郵便番号/2･･･住所)：");
+        			input =scanner.nextLine();
+        	
+        	
+        			if ("1".equalsIgnoreCase(input)) {
+        				System.out.println("郵便番号検索クラスが入る予定");
+        				
+        				//var post = new PostSerach()
+        				
+        				
+        				//↓Sortクラスデータを表示するクラスを入れる
+                		
+                		
+            			//postのデータか住所のデータをでヒットしたものを受け取る。    		
+            		
+            		
+        				//↑Sortクラスデータを表示するクラス
+        				
+        				break;
+        			} else if ("2".equalsIgnoreCase(input)) {
+        				System.out.println("住所検索クラスが入る予定");
+        				
+        				//var Adress = new AdressSerach()
+        				
+        				
+        				//↓Sortクラスデータを表示するクラスを入れる
+                		
+                		
+            			//postのデータか住所のデータをでヒットしたものを受け取る。    		
+            		
+            		
+        				//↑Sortクラスデータを表示するクラス
+        				  				
+        				break;
+        			} else {
+        				System.out.println("無効な入力です。1 または 2 を選択してください。");
+        			}
+           
+        		}
+        	//いらんかも↓
+        		//↓Sortクラスデータを表示するクラスを入れる
+        		
+        		
+        			//postのデータか住所のデータをでヒットしたものを受け取る。    		
+        		
+        		
+        		//↑Sortクラスデータを表示するクラス
+        	//いらんかも↑
+             
+
+        	//↓もう一度検索するかどうかを聞きyesならまた郵便検索か住所検索かまで戻る
+        	while (true) { 
+        		
+        		System.out.println("新たな条件で検索しますか？(y/n)：");
+        		String reinput = "";
+        		reinput =scanner.nextLine();  		
+
+        		if ("y".equalsIgnoreCase(reinput)) {
+        			System.out.println("続ける");
+        			break;
+        		} else if ("n".equalsIgnoreCase(reinput)) {
+        			System.out.println("続けない");
+        			scanner.close();
+        			return;
+        		} else {
+        			System.out.println("無効な入力です。y または n を選択してください。");
+
+        		}
+        
+        	}       	
+        	
+        }
+        
+	}
+
+}
