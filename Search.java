da3257b5c64f5299ba66bb8922442004a941230a
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2025-07-03 09:37:36 +0900

    Create Search.java

---
+++ Search.java
@@ -0,0 +1,25 @@
+package Final;
+
+//親クラスにしようか迷っているクラス（PostSerach,AddressSerachの基底クラスにする予定）
+
+
+public class Search {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
